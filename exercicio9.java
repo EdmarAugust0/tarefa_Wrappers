40cfecfa595f01914428d7e4fa2287dcb1f62422
EdmarAugust0 <edmar.contato149@gmail.com>
2024-08-22 14:18:09 -0300

    :tada: Commit inicial

---
+++ exercicio9.java
@@ -0,0 +1,16 @@
+package edmar;
+
+public class exercicio9 {
+	
+	public static void main(String args[]) {
+		pToW();
+	}
+	
+	public static void pToW() {
+		int numP = 10;
+		System.out.println(numP);
+		
+		Integer	numW = numP;
+		System.out.println(numW);
+	}
+}
